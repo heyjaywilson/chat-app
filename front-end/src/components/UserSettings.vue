847ed6b16f430ccaba1ff069631e65e13a238c1a
Maegan Wilson <maegan@maeganwilson.com>
2017-12-05 10:24:16 -0600

    Adding user settings

---
+++ front-end/src/components/UserSettings.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>User Settings</h2>
+    <label>Username</label><input type="text" placeholder="Username" v-model="username"><br>
+    <button v-on:click="save">Apply changes</button>
+  </div>
+</template>
+<script>
+import firebase from 'firebase'
+
+let user = firebase.auth().currentUser;
+
+export default {
+  name: 'UserSettings',
+  data: function() {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    save: function(){
+      user.updateProfile({displayName: this.username}).then(
+          () => {
+            alert("Your profile has been updated.")
+            },
+          (err) => {alert('Oops. '+ err.message)}
+      );
+    }
+  }
+}
+</script>
