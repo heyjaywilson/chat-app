22ae4cbd82b09e5ba0488392588121726f18a3f6
Maegan Wilson <maegan@maeganwilson.com>
2017-12-04 22:27:17 -0600

    login component

---
+++ front-end/src/components/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login">
+      <h3>Sign In</h3>
+      <input type="text" placeholder="Email"><br>
+      <input type="text" placeholder="Password"><br>
+      <button>Connect</button>
+      <p>You don't have an account? You can create one</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: function() {
+      return {}
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+
+</style>
