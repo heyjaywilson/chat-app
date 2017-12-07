a299029534f93c232469cd83c99a60ada47f654b
Maegan Wilson <maegan@maeganwilson.com>
2017-12-06 22:12:27 -0600

    created template

---
+++ front-end/src/components/SideMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="s">
+    <h2>Menu</h2>
+    <ul>
+      <li v-for="item in links">{{item}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SideMenu',
+  data() {
+    return {
+      links: ['Profile', 'All Chats', 'Settings', 'Log out']
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
