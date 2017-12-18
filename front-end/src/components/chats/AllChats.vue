95ab959fb8ee09026ddf5d70f30708fb667261c4
Maegan Wilson <maegan@maeganwilson.com>
2017-12-18 13:00:15 -0600

    Added styling for inputs
    Signed-off-by: Maegan Wilson <maegan@maeganwilson.com>

--- front-end/src/components/chats/AllChats.vue
+++ front-end/src/components/chats/AllChats.vue
@@ -1,42 +1,43 @@
 <template>
-  <div class="chats m">
-    <!--list of chats and the ability to add new chats-->
-    <section class="list">
-      <!-- CREATE NEW CHAT -->
-      <h1>create new chat</h1>
-      <label>Chat name: </label><input type="text" v-model="chatName"><br>
-      <button type="button" v-on:click="addNewChat">Add Chat</button>
+<div class="chats m">
+  <!--list of chats and the ability to add new chats-->
+  <section class="list">
+    <!-- CREATE NEW CHAT -->
+    <h1>create new chat</h1>
+    <label>Chat name: </label><input type="text" v-model="chatName" class="newChat"><br>
+    <button type="button" v-on:click="addNewChat">Add Chat</button>
 
-      <!-- LIST OF CHATS -->
-      <h1>all public chats</h1>
-      <ul>
-        <li v-for="chat in showAll">{{chat.name}}</li>
-      </ul>
-    </section>
+    <!-- LIST OF CHATS -->
+    <h1>all public chats</h1>
+    <ul>
+      <li v-for="chat in showAll">{{chat.name}}</li>
+    </ul>
+  </section>
 
-    <!--Chat display-->
-    <section class="msg">
-      <button type="button" name="button" v-on:click = "getMessages">Get chats</button>
+  <!--Chat display-->
+  <section class="msg">
+    <button type="button" name="button" v-on:click="getMessages">Get chats</button>
 
-      <h1 class="chatname">{{showCurrentChat[0]}}</h1>
-
-      <Message v-for="mess in showAllMessages" :key="mess.id" :message="mess.message" :sender="mess.sender"></Message>
-
-      <input type="text" v-model="message"><button type="button" v-on:click="send">Send</button>
-    </section>
-  </div>
+    <h1 class="chatname">{{showCurrentChat[0]}}</h1>
+    <input type="text" v-model="message" class="messageBox"><button type="button" v-on:click="send">Send</button>
+    <Message v-for="mess in showAllMessages" :key="mess.id" :message="mess.message" :sender="mess.sender"></Message>
+  </section>
+</div>
 </template>
 
 <script>
 import Message from './Message'
 
-import { mapGetters, mapMutations } from 'vuex'
+import {
+  mapGetters,
+  mapMutations
+} from 'vuex'
 
 export default {
   name: 'AllChats',
-  components:{
+  components: {
     'Message': Message
-  } ,
+  },
   computed: {
     ...mapGetters([
       'showName', 'showAll', 'showCurrentChat',
@@ -46,30 +47,32 @@ export default {
   data() {
     return {
       chatName: '',
-      message:''
+      message: ''
     }
   },
   methods: {
     ...mapMutations([
       'getAllChats', 'addChat', 'emptyChats',
       'setInitialChat', 'changeChat', 'sendMessage',
-      'setDisplayName','getAllMessages'
+      'setDisplayName', 'getAllMessages'
     ]),
-    addNewChat: function(){
+    addNewChat: function() {
       this.addChat(this.chatName)
     },
-    send: function(){
+    send: function() {
       console.log(this.showName)
       this.sendMessage({
         id: this.showCurrentChat[1],
         message: this.message,
         user: this.showName
-        })
+      })
       console.log('Message being sent: ' + this.message)
-      this.getAllMessages({id:this.showCurrentChat[1]})
+      this.getAllMessages({
+        id: this.showCurrentChat[1]
+      })
       this.message = ''
     },
-    getMessages: function(){
+    getMessages: function() {
       this.getAllMessages({
         id: this.showCurrentChat[1]
       })
@@ -87,19 +90,34 @@ ul {
 .chats {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 5%;
-  grid-template-areas:
-  "l msg msg .";
+  grid-template-areas: "l msg msg .";
   grid-column-gap: 10px;
 }
+
+.newChat {
+  padding: 5px;
+}
+
 .list {
   grid-area: l;
+  text-align: left;
 }
+
 .msg {
   grid-area: msg;
   border: 2px black solid;
   border-radius: 5px;
+  padding: 10px;
+  text-align: left;
 }
+
 .chatname {
   color: blue;
+}
+
+.messageBox {
+  width: 80%;
+  padding: 5px;
+  margin-right: 5px;
 }
 </style>
