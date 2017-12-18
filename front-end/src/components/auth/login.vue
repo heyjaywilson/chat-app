5e4a66f56e2c025aab25d0c5e93bbc434b3b6fd4
Maegan Wilson <maegan@maeganwilson.com>
2017-12-17 19:52:33 -0600

    Added login with persist option
    Signed-off-by: Maegan Wilson <maegan@maeganwilson.com>

--- front-end/src/components/auth/login.vue
+++ front-end/src/components/auth/login.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="login m">
       <h3>Log In</h3>
+      <label>Email: </label>
       <input type="text" placeholder="Email" v-model="email"><br>
+
+      <label>Password: </label>
       <input type="password" placeholder="Password" v-model="password"><br>
+
+      <label>Check this box if you don't want to log off by closing a window </label>
+      <input type="checkbox" v-model="persist"><br>
+
       <button v-on:click="login">Connect</button><button v-on:click="google">Login with Google</button>
       <p>You don't have an account? You can <router-link to="/sign-up">create one</router-link></p>
   </div>
@@ -18,12 +25,16 @@ export default {
   data: function() {
       return {
         email: '',
-        password: ''
+        password: '',
+        persist: false
       }
   },
   methods: {
     google: function() {
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(
+      console.log(this.persist)
+
+      if(this.persist) {
+      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(
         firebase.auth().signInWithPopup(provider).then(
           (user)=> {
               this.$router.replace('profile')
@@ -32,11 +43,25 @@ export default {
             alert('Oops. '+ err.message)
           }
         )
-      )
+      )}
+
+      else {
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(
+          firebase.auth().signInWithPopup(provider).then(
+            (user)=> {
+                this.$router.replace('profile')
+            },
+            (err) => {
+              alert('Oops. '+ err.message)
+            }
+          )
+        )
+      }
     },
     login: function() {
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(
-      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+      if(this.persist){
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(
+          firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
         (user) => {
           if (user == null) {
             this.$router.replace('newuser')
@@ -50,6 +75,23 @@ export default {
         }
       ))
     }
+    else {
+      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(
+    firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+      (user) => {
+        if (user == null) {
+          this.$router.replace('newuser')
+        }
+        else {
+          this.$router.replace('profile')
+        }
+      },
+      (err) => {
+        alert('Oops. ' + err.message)
+      }
+    ))
+    }
+  }
   }
 }
 </script>
@@ -58,7 +100,7 @@ export default {
 .login {
   margin-top: 40px;
 }
-input {
+input{
   margin: 10px 0;
   width: 50%;
   padding: 15px;
