c06ea64809db2983bdbf493b1de20dd77b81dfa5
Maegan Wilson <maegan@maeganwilson.com>
2017-12-29 13:48:02 -0600

    Formatting and working on getting user chats
    Signed-off-by: Maegan Wilson <maegan@maeganwilson.com>

--- front-end/src/store/user.js
+++ front-end/src/store/user.js
@@ -1,16 +1,19 @@
-import fire from '../config'
+import fire from "../config";
+
+const conversations = fire.db.collection("conversations");
 
 /** State**/
 const state = {
-  username: '',
-  user_id: ''
-}
+  username: "",
+  user_id: "",
+  user_chats: []
+};
 
 /** Getters**/
 const getters = {
   showName: state => state.username,
   showID: state => state.user_id
-}
+};
 
 /**Mutations**/
 const mutations = {
@@ -18,27 +21,45 @@ const mutations = {
    * All used to update user names and IDs
    */
   setDisplayName(state) {
-    state.username = fire.auth.currentUser.displayName
+    state.username = fire.auth.currentUser.displayName;
   },
   setUserID(state) {
-    state.user_id = fire.auth.currentUser.uid
+    state.user_id = fire.auth.currentUser.uid;
   },
-}
+  addChat(state, chat) {
+    state.user_chats.push({ name: chat.name, id: chat.id });
+  }
+};
 
 const actions = {
-  changeDisplayName({commit}, name){
-    fire.auth.currentUser.updateProfile({displayName: name}).then(
-      ()=> {
-        commit('setDisplayName')
+  changeDisplayName({ commit }, name) {
+    fire.auth.currentUser.updateProfile({ displayName: name }).then(
+      () => {
+        commit("setDisplayName");
       },
-      (err)=> {alert('Oops '+err.message)}
-    )
+      err => {
+        alert("Oops " + err.message);
+      }
+    );
+  },
+  getUserChats({ commit }) {
+    conversations
+      .where("name", "==", state.username)
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          console.log(doc.id + " " + doc.name);
+        });
+      })
+      .catch(function(error) {
+        console.log("Oops. " + error.message);
+      });
   }
-}
+};
 
 export default {
   state,
   getters,
   mutations,
   actions
-}
+};
